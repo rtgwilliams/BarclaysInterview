a8b938605f5fbb0b8562b5f7cbea58f3f7a2a80f
williamsrh <rhys.williams@thehutgroup.com>
2026-06-14 20:38:28 +0100

    Remove unnecessary logic

--- src/main/java/com/example/eaglebank/service/UserService.java
+++ src/main/java/com/example/eaglebank/service/UserService.java
@@ -1,18 +1,15 @@
 package com.example.eaglebank.service;
 
 import java.time.Instant;
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
-import com.example.eaglebank.exception.BadRequestException;
 import com.example.eaglebank.exception.UserHasBankAccountsException;
 import com.example.eaglebank.exception.UserNotFoundException;
 import com.example.eaglebank.model.domain.NewUser;
 import com.example.eaglebank.model.domain.ProcessedUser;
 import com.example.eaglebank.model.domain.UserUpdate;
-import com.example.eaglebank.model.json.BadRequestErrorResponse.ValidationError;
 import com.example.eaglebank.repository.BankAccountRepository;
 import com.example.eaglebank.repository.UserRepository;
 
@@ -27,12 +24,6 @@ public class UserService {
     }
 
     public ProcessedUser createUser(final NewUser newUser) {
-        if (userRepository.findByEmail(newUser.getEmail()) != null) {
-            throw new BadRequestException(
-                    "Invalid details supplied",
-                    List.of(new ValidationError("email", "already belongs to a user", "duplicate")));
-        }
-
         final Instant now = Instant.now();
         final ProcessedUser processedUser = new ProcessedUser(
                 generateUserId(),
@@ -68,15 +59,6 @@ public class UserService {
 
     public ProcessedUser updateUser(final String userId, final UserUpdate userUpdate) {
         final ProcessedUser existingUser = getUser(userId);
-        if (userUpdate.getEmail() != null && !userUpdate.getEmail().equalsIgnoreCase(existingUser.getEmail())) {
-            final ProcessedUser userWithEmail = userRepository.findByEmail(userUpdate.getEmail());
-            if (userWithEmail != null) {
-                throw new BadRequestException(
-                        "Invalid details supplied",
-                        List.of(new ValidationError("email", "already belongs to a user", "duplicate")));
-            }
-        }
-
         final ProcessedUser updatedUser = new ProcessedUser(
                 existingUser.getId(),
                 userUpdate.getName() == null ? existingUser.getName() : userUpdate.getName(),
